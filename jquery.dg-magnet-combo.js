0dda5a079aa8ba311bb56e710097c658aa8f032f
Dharmavirsinh Jhala <dharmavir@gmail.com>
2012-06-03 21:15:08 +0530

    jQuery 1.6 compatibility, versioning and history
    - for compatibility with jQuery 1.6 and newer, replaced attr() with new
    function prop().
    - corrected version info in README and added revision history.
    - added TODO and Revision History

--- jquery.dg-magnet-combo.js
+++ jquery.dg-magnet-combo.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery DG Magnet Combo 1.1
+ * jQuery DG Magnet Combo 1.2
  * http://www.digitss.com/
  * http://blogs.digitss.com/projects/jquery-plugins/
  *
@@ -8,13 +8,22 @@
  * Copyright 2011, DiGiTSS
  * Released under the MIT, BSD, and GPL Licenses.
  *
- * Release Date: Mon Jan 26 2011
+ * Initial Release Date: Mon Jan 26 2011
+ * Last Updated Date: Sat Feb 12 2011
  */
 // TODO: Make it work with Shift key naturally
 jQuery.fn.extend({
 dgMagnetCombo: function()
 {
+	$.each($(this), function(){
+		$(this).buildMagnetCombo();
+	});
+}
+,
+buildMagnetCombo: function()
+{
 	var bControlPress; // Maintain Control state	
+	var dataKey = "data_" + $(this).attr("id");
 	$(this).data("dgVal", $(this).val() || []); // Set selected values on load if any (reported by fflavio)
 	$(this).click( function()
 	{
@@ -57,7 +66,7 @@ dgMagnetCombo: function()
 
 	$(this).keydown(function(e)
 	{
-		if(e.which == 17 || e.which == 16)	// CTL, Shift Key check
+		if(e.which == 17 || e.which == 16 || e.which == 18)	// CTL, Shift Key check
 		{
 			bControlPress = true;
 		}
@@ -67,9 +76,10 @@ dgMagnetCombo: function()
 	$(this).keyup( function(e)
 	{
 		var sTop	=	this.scrollTop;
-		if(e.which == 17 || e.which == 16)	// CTL, Shift Key check
+
+		if(e.which == 17 || e.which == 16 || e.which == 18)	// CTL, Shift Key check
 		{
-			var oVal = $(this).data("dgVal") || [];
+			var oVal = $(this).data(dataKey) || [];
 			if(oVal.length > 0)
 			{
 				$.each($(this).val(), function(index, value)
@@ -84,18 +94,18 @@ dgMagnetCombo: function()
 			bControlPress = false;
 		}
 		else
-		{
+		{		
 			if(!(e.which >= 37 && e.which <= 40))	// To make Up and Down arrow work
 			{
-				$(this).click();
+				//$(this).click();
 			}
 			else
-			{
-				$(this).val($(this).data("dgVal"));
+			{			
+				$(this).val($(this).data(dataKey));
 			}
 		}
 		this.scrollTop = sTop;
-		return false;
+		return true;
 	});
 }
 });
